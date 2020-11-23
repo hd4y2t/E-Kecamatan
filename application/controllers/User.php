11d0c0d53854fcbf0c2f57e8e0a280b1529fd5a2
hd4y2t <hd4y2t@gmail.com>
2020-11-23 15:08:03 +0700

    edit dashboard user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,7 +6,9 @@ class User extends CI_Controller
     public function index()
     {
         $data['user'] = $this->db->get_where('user', ['ni' => $this->session->userdata('ni')])->row_array();
-
+        $data['title'] = 'Dashboard';
+        $this->load->view('templates/auth_header', $data);
         $this->load->view('user/index');
+        $this->load->view('templates/auth_footer');
     }
 }
