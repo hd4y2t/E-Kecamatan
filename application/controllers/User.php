db06443d452f1a28e2bdfed0867a2f633c3d4331
hd4y2t <hd4y2t@gmail.com>
2021-02-01 21:51:41 +0700

    fixed potition table in edit profile

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -39,7 +39,7 @@ class User extends CI_Controller
     {
         $data['user'] = $this->db->get_where('user', ['ni' => $this->session->userdata('ni')])->row_array();
         $data['title'] = 'Edit Profile';
-        $this->form_validation->set_rules('name', 'Nama lengkap', 'required|trim');
+        $this->form_validation->set_rules('nama', 'Nama lengkap', 'required|trim');
 
         if ($this->form_validation->run() == false) {
             $this->load->view('templates/header', $data);
@@ -50,11 +50,26 @@ class User extends CI_Controller
         } else {
             $ni = $this->input->post('ni');
             $nama = $this->input->post('nama');
+            //cek jika ada gambar yang akan di upload
+            $upload_image = $_FILES['foto']['name'];
+            if ($upload_image) {
+                $config['upload_path'] = './assets/img/profile/';
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_size']     = '2048';
+
+                $this->load->library('upload', $config);
+                if ($this->upload->do_upload('foto')) {
+                    $new_image = $this->upload->data('file_name');
+                    $this->db->set('foto', $new_image);
+                } else {
+                    echo $this->upload->display_errors();
+                }
+            }
 
             $this->db->set('nama', $nama);
             $this->db->where('ni', $ni);
             $this->db->update('user');
-            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">' . $this->upload->display_errors() . '</div>');
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Profile berhasil diedit</div>');
             redirect('user/profile');
         }
     }
--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -1,37 +1,42 @@
 <div class="content" data-color="green" data-background-color="white" data-image="<?= base_url(); ?>assets/img/sidebar-2.jpg">
     <div class="container-fluid">
         <div class="row ">
-            <div class="col-lg-6">
+            <div class="col-lg-7">
                 <div class="content">
                     <div class="container-fluid">
-                        <div class="card">
+                        <div class="card mb-3">
                             <div class="card-header card-header-success">
                                 <h3 class="card-title"><?= $title ?></h3>
                             </div>
                             <div class="card-body">
                                 <div class="row">
-                                    <div class="col">
-                                        <?= $this->ssession->flashdata('message'); ?>
-                                        <div class="row">
-                                            <img src="<?= base_url('assets/img/profile/' . $user['foto']); ?>" alt="..." class="img-thumbnail">
-                                            <div class="card-header card-header">
-                                                <h3 class="card-title"> Nama : <?= $user['nama']; ?></h3>
-
-                                                <h4 class="card-title"> Nik : <?= $user['ni']; ?></h4>
-                                            </div>
-
+                                    <?= $this->session->flashdata('message'); ?>
+                                    <div class="row-md-2">
+                                        <img height="30%" width="30%" src="<?= base_url('assets/img/profile/' . $user['foto']); ?>" class="img-thumbnail">
+                                        <div class="card-header card-header">
+                                            <h3 class="card-title"> Nama : <?= $user['nama']; ?></h3>
+                                            <h4 class="card-title"> Nik : <?= $user['ni']; ?></h4>
+                                            <h4 class="card-title"> alamat : </h4>
+                                            <h4 class="card-title"> Kelurahan : </h4>
+                                            <h4 class="card-title"> RT : </h4>
+                                            <h4 class="card-title"> RW : </h4>
                                         </div>
-                                        Tanggal mendaftar : <h4 class="card-title"><?= date('d F Y', $user['date_create']); ?></h4>
                                     </div>
+
                                 </div>
                             </div>
-
+                            <p>Tanggal mendaftar :</p>
+                            <h4 class="card-title"><?= date('d F Y', $user['date_create']); ?></h4>
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
+</div>
+</div>
+</div>
 
 </div>
 </div>
