1d57f6ba75a37f46c1203ef1390a5cbc2e4d555a
hd4y2t <hd4y2t@gmail.com>
2021-02-08 20:34:07 +0700

    change file in pegawai/index

--- application/views/pegawai/index.php
+++ application/views/pegawai/index.php
@@ -1,13 +1,41 @@
 <div class="content" data-color="green" data-background-color="white" data-image="<?= base_url(); ?>assets/img/sidebar-2.jpg">
     <div class="container-fluid">
-        <div class="row ">
-            <p> <?= $title; ?></p>
-        </div>
 
+        <div class="row ">
+            <div class="col-lg-7">
+                <div class="content">
+                    <div class="container-fluid">
+                        <div class="row">
+
+                            <div class="card">
+                                <div class="card-header card-header-success">
+                                    <h3 class="card-title"><?= $title; ?></h3>
+                                </div>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col">
+                                            <table class="table table-hover">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col"></th>
+                                                        <th scope="col">Perihal</th>
+                                                        <th scope="col">Action</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
-
-<div class="row">
-
 </div>
 </body>
