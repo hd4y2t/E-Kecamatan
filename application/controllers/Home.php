8335ffdc3738f2d62d0f80a0366bd65501de9b80
hd4y2t <hd4y2t@gmail.com>
2021-03-20 14:57:53 +0700

    ganti tampilan awal

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,25 +7,34 @@ class Home extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('galery_model', 'galery');
+        // $this->load->model('galery_model', 'galery');
 
-        $this->load->helper(array('form', 'url', 'Cookie', 'String'));
-        $this->load->library('form_validation');
+        // $this->load->helper(array('form', 'url', 'Cookie', 'String'));
+        // $this->load->library('form_validation');
     }
 
     public function index()
     {
         // $data = $this->dashboard->user();
-        $data['profile'] = $this->galery->profil();
-        $judul = [
-            'title' => 'Home - Kecamatan Sematang Borang',
-            'sub_title' => ''
-        ];
+        // $data['profile'] = $this->galery->profil();
+        $data['title'] = 'E-Kecamatan';
 
         // $data['sm'] = $this->db->get('surat_masuk')->row_array();
         // var_dump($data);
-        $this->load->view('frontend/header', $judul);
+        $this->load->view('home/header', $data);
+        $this->load->view('home/index', $data);
+        $this->load->view('home/footer');
+    }
+    public function contoh()
+    {
+        // $data = $this->dashboard->user();
+        $data['profile'] = $this->galery->profil();
+        $data['title'] = 'E-Kecamatan';
+
+        // $data['sm'] = $this->db->get('surat_masuk')->row_array();
+        // var_dump($data);
+        $this->load->view('frontend/header', $data);
         $this->load->view('frontend/home', $data);
-        $this->load->view('frontend/footer', $data);
+        $this->load->view('frontend/footer');
     }
 }
---
+++ application/views/home/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>
+        <?= $title; ?>
+    </title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link rel="icon" type="image/png" href="<?= base_url('assets/'); ?>img/favicon.gif">
+    <link href="<?= base_url(); ?> assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/icofont/icofont.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/boxicons/css/boxicons.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/remixicon/remixicon.css">
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/venobox/venobox.css">
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/owl.carousel/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/aos/aos.css">
+
+    <!-- Template Main CSS File -->
+    <link href="<?= base_url('assets/') ?>css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Bethany - v2.2.1
+  * Template URL: https://bootstrapmade.com/bethany-free-onepage-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
