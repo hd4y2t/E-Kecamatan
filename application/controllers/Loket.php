634e4092cf677fb8e843b743265ac971abd100ee
hd4y2t <hd4y2t@gmail.com>
2021-03-30 20:35:04 +0700

    edit antrian

---
+++ application/controllers/Loket.php
@@ -0,0 +1,87 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Camat extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        // $this->load->model('Madmin');
+
+        $this->load->model('Mcamat');
+        $this->load->library('form_validation');
+        is_logged_in();
+    }
+
+    public function index()
+    {
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $data['title'] = 'Dashboard';
+        $data['warga'] = $this->db->get('penduduk')->num_rows();
+        $data['antrian'] = $this->db->get('pengajuan_surat')->num_rows();
+        $data['antrian_non'] = $this->db->get_where('pengajuan_surat', ['status !=' => 5])->num_rows();
+        $data['antrian_done'] = $this->db->get_where('pengajuan_surat', ['status' => 5])->num_rows();
+        $data['penduduk'] = $this->db->get('penduduk')->num_rows();
+        $data['user_non'] = $this->db->get('user')->num_rows();
+        // $this->db->where('is_active =', 0);
+        $data['status'] = [
+            1 => 'Pending',
+            2 => 'Syarat Tidak Terpenuhi',
+            3 => 'Diterima dan Dilanjutkan',
+
+        ];
+        $status = $this->input->post('status');
+        $this->load->model('Mcamat', 'camat');
+
+        $data['surat_keluar'] = $this->camat->getSurat();
+        $data['surat'] = $this->db->get('surat')->result_array();
+
+
+        // $this->form_validation->set_rules('no_surat', 'Nomor Surat', 'required');
+        // $this->form_validation->set_rules('jenis', 'Jenis Surat', 'required');
+        // $this->form_validation->set_rules('nm_surat_keluar', 'Nama Surat', 'required');
+        // $this->form_validation->set_rules('tgl', 'Tanggal', 'required');
+        // $this->form_validation->set_rules('keterangan', 'Keterangan', 'required');
+        // $this->form_validation->set_rules('file_surat', 'Keterangan', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/navbar', $data);
+            $this->load->view('loket/index', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $no_surat =  $this->input->post("no_surat", TRUE);
+            $jenis =  $this->input->post("jenis", TRUE);
+            $nm_surat_keluar =  $this->input->post("nm_surat_keluar", TRUE);
+            $tgl =  $this->input->post("tgl", TRUE);
+            $keterangan =  $this->input->post("keterangan", TRUE);
+            // $file_surat =  $this->input->post("file_surat", TRUE);
+
+            $config['upload_path']          = './upload/surat_masuk';
+            $config['allowed_types']        = 'pdf|doc|docx';
+            $this->load->library('upload', $config);
+
+            if ($this->upload->do_upload('file_surat')) {
+
+                $data = array('upload_data' => $this->upload->data());
+                $file_surat = $data['upload_data']['file_name'];
+
+                $save = [
+                    'id' => '',
+                    'no_surat' => $no_surat,
+                    'jenis' => $jenis,
+                    'nm_surat_keluar' => $nm_surat_keluar,
+                    'tgl' => date('Y-m-d', strtotime($tgl)),
+                    'keterangan' => $keterangan,
+                    'file' => $file_surat
+                ];
+
+                $this->db->insert('surat_keluar', $save);
+                $this->session->set_flashdata('success', 'Berhasil Ditambahkan!');
+                redirect(base_url("pegawai/surat_masuk"));
+            }
+        }
+    }
+}
---
+++ application/views/loket/index.php
@@ -0,0 +1,186 @@
+<div class="content" data-color="#d6fad6" data-background-color="white" data-image="<?= base_url(); ?>assets/img/sidebar-2.jpg">
+    <div class="container-fluid">
+
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="card card-stats">
+                            <div class="card-header card-header-danger card-header-icon">
+                                <div class="card-icon">
+                                    <i class="material-icons">info_outline</i>
+                                </div>
+                                <p class="card-category">Antrian Surat yang belum dicek</p>
+                                <h3 class="card-title"><?= $antrian_non; ?></h3>
+                            </div>
+                            <div class="card-footer">
+                                <div class="stats">
+                                    <i class="material-icons text-danger">warning</i>
+                                    Antrian yang belum di tangani
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="card card-stats">
+                            <div class="card-header card-header-warning card-header-icon">
+                                <div class="card-icon">
+                                    <i class="material-icons">content_copy</i>
+                                </div>
+                                <p class="card-category">Antrian Surat yang ada</p>
+                                <h3 class="card-title"><?= $antrian; ?>
+                                </h3>
+                            </div>
+                            <div class="card-footer">
+                                <div class="stats">
+                                    <i class="material-icons">local_offer</i> Tracked from Github
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="card card-stats">
+                            <div class="card-header card-header-success card-header-icon">
+                                <div class="card-icon">
+                                    <i class="fa fa-check-square"></i>
+                                </div>
+                                <p class="card-category">Antrian Surat yang selesai</p>
+                                <h3 class="card-title"><?= $antrian_done; ?></h3>
+                            </div>
+                            <div class="card-footer">
+                                <div class="stats">
+                                    <i class="material-icons">date_range</i> Last 24 Hours
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="card card-stats">
+                            <div class="card-header card-header-info card-header-icon">
+                                <div class="card-icon">
+                                    <i class="fa fa-users"></i>
+                                </div>
+                                <p class="card-category">Penduduk yang memakai sistem</p>
+                                <h3 class="card-title"><?= $warga; ?></h3>
+                            </div>
+                            <div class="card-footer">
+                                <div class="stats">
+                                    <i class="material-icons">update</i> Just Updated
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg">
+                        <div class="content">
+                            <div class="container-fluid">
+                                <div class="card">
+                                    <div class="card-header card-header-success">
+                                        <h3 class="card-title">Surat Keluar</h3>
+                                    </div>
+                                    <div class="card-body">
+                                        <?= form_error('surat', '<div class="text-danger" surat="alert">', '</div>'); ?>
+                                        <?= $this->session->flashdata('message'); ?>
+                                        <!-- <a href="" class="btn btn-success" data-toggle="modal" data-target="#newsuratModal">Tambah Surat</a> -->
+                                        <div class="row">
+                                            <div class="col">
+                                                <div class="table-responsive">
+                                                    <table class="table table-hover" id="myTable">
+                                                        <thead class="text-success">
+
+                                                            <tr>
+                                                                <th scope="col"></th>
+                                                                <th scope="col">Nomor Surat</th>
+                                                                <th scope="col">Nama </th>
+                                                                <th scope="col">Jenis</th>
+                                                                <th scope="col">Surat</th>
+                                                                <th scope="col">Tanggal</th>
+                                                                <th scope="col">Keterangan</th>
+                                                                <!-- <th scope="col">File</th> -->
+                                                                <th scope="col">Action</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <?php $i = 1; ?>
+                                                            <?php foreach ($surat_keluar as $s) : ?>
+                                                                <tr>
+                                                                    <th scope="row"><?= $i ?></th>
+                                                                    <td><?= $s['no_surat']; ?></td>
+                                                                    <td><?= $s['nm_surat_keluar']; ?></td>
+                                                                    <td><?= $s['jenis']; ?></td>
+                                                                    <td><?= $s['nm_surat']; ?></td>
+                                                                    <td><?= $s['tgl']; ?></td>
+                                                                    <td><?= $s['keterangan']; ?></td>
+                                                                    <!-- <td>
+                                                                        <button class="btn btn-simple btn-info btn-sm" data-toggle="modal" data-target="#lihatSurat<?= $s['id']; ?>"><i class="material-icons">remove_red_eye</i></button>
+                                                                    </td> -->
+                                                                    <td>
+                                                                        <button class="btn btn-simple btn-success btn-icon btn-sm" data-toggle="modal" data-target="#statusPengajuan<?= $s['id']; ?>"><i class="material-icons">outbond</i>Status</button>
+
+                                                                    </td>
+                                                                    <?php $i++; ?>
+                                                                </tr>
+                                                            <?php endforeach ?>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php foreach ($surat_keluar as $m) : ?>
+    <div class="modal fade" id="statusPengajuan<?= $m['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg ">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="material-icons">clear</i></button>
+                </div>
+
+                <form method="post" action="<?= base_url(); ?>camat/paraf/<?= $m['id']; ?>">
+                    <div class="modal-body text-center">
+                        <h5>Update Status Pengajuan ID: <?= $m['id'] ?>? </h5>
+                        <label for="status">Pilih Status</label>
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="status" value="1" <?= $m['status'] == '1' ? 'checked="true"' : '' ?>><span class="circle"></span><span class="check"></span> <?= $status['1'] ?>
+                            </label>
+                            <label>
+                                <input type="radio" name="status" value="2" <?= $m['status'] == '2' ? 'checked="true"' : '' ?>><span class="circle"></span><span class="check"></span> <?= $status['2'] ?>
+                            </label>
+                            <label>
+                                <input type="radio" name="status" value="3" <?= $m['status'] == '3' ? 'checked="true"' : '' ?>><span class="circle"></span><span class="check"></span> <?= $status['3'] ?>
+                            </label>
+                            <label>
+                                <input type="radio" name="status" value="4" <?= $m['status'] == '4' ? 'checked="true"' : '' ?>><span class="circle"></span><span class="check"></span> <?= $status['4'] ?>
+                            </label>
+
+                            <label>
+                                <input type="radio" name="status" value="5" <?= $m['status'] == '5' ? 'checked="true"' : '' ?>><span class="circle"></span><span class="check"></span> <?= $status['5'] ?>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="modal-footer text-center">
+                        <button type="button" class="btn btn-simple" data-dismiss="modal">Tidak</button>
+                        <button type="submit" class="btn btn-info btn-simple">Update</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+<?php endforeach; ?>
+</body>
